34c8000e4ac06d48b3d023ccd9a04af16edd357c
Emilien Macchi <emilien.macchi@enovance.com>
2013-08-04 14:22:19 +0200

    Fix some issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,18 +16,14 @@ sudo sed -i '/^%sudo/ { s/ALL$/NOPASSWD:ALL/ }' /etc/sudoers
 
 sudo update-alternatives --set editor /usr/bin/vim.basic
 
-
-sudo apt-get -y install ufw && \
-    sudo ufw allow proto tcp from any to any port 22 && \
-    sudo ufw -f enable
-
 cd $HOME
 mkdir -p git
 cd git
 
 sudo chsh -s /bin/zsh $USER
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-git clone https://github.com/sontek/dotfiles/; cd dotfiles; ./install.sh;
+git clone https://github.com/sontek/dotfiles/
+cd dotfiles && ./install.sh
 cd ..
 
 git clone https://github.com/EmilienM/emilien-config
@@ -35,9 +31,4 @@ git clone https://github.com/EmilienM/emilien-config
 for f in gitconfig screenrc vimrc zshrc;do
     rm -f ~/.${f}
     ln -fs emilien-config/rc-config/${f} ~/.${f}
-done
-
-
-for f in screenrc vimrc zshrc;do
-   source ~/.${f}
 done
